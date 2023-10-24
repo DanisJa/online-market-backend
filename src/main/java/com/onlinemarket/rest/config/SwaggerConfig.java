a4860dc3d1b9b67887fb94bcddebb5e54f43d7ef
DanisJa <danisjusic@icloud.com>
2023-10-24 14:28:37 +0200

    feat: swagger configured, mongoDB configured, application.yml configured, new DB 'online-market' added

---
+++ src/main/java/com/onlinemarket/rest/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.onlinemarket.rest.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "${configuration.swagger.app}", version = "0.0.1", description = "${configuration.swagger.description}"),
+        security = {@SecurityRequirement(name="key")},
+        servers = {@Server(url = "/", description = "Default Server URL")}
+)
+public class SwaggerConfig {}
