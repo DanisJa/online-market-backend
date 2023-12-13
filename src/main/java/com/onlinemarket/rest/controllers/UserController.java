622d5069648b811796ff54bd1cddf905761b1c04
DanisJa <danisjusic@icloud.com>
2023-12-13 20:52:14 +0100

    user controller tests added, 1/5 passing

--- src/main/java/com/onlinemarket/rest/controllers/UserController.java
+++ src/main/java/com/onlinemarket/rest/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.onlinemarket.rest.controllers;
 
+import com.onlinemarket.core.exceptions.repository.ResourceNotFoundException;
 import com.onlinemarket.core.service.UserService;
+import com.onlinemarket.rest.dto.responses.ApiResponse;
 import com.onlinemarket.rest.dto.user.UserDTO;
 import com.onlinemarket.rest.dto.user.UserRequestDTO;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -20,15 +22,31 @@ public class UserController {
     public UserController(UserService userService) {this.userService = userService;}
 
     @GetMapping
-    public List<UserDTO> findAll(){
-        return userService.findAll();
+    public ResponseEntity<ApiResponse<List<UserDTO>>> findAll(){
+        return ResponseEntity.ok(new ApiResponse<>(true, userService.findAll()));
     }
 
     @GetMapping("/{id}")
-    public UserDTO findById(@PathVariable String id) {return userService.findById(id);}
+    public ResponseEntity<ApiResponse<UserDTO>> findById(@PathVariable String id) {
+        try {
+            return ResponseEntity.ok(new ApiResponse<>(true, userService.findById(id)));
+        } catch(ResourceNotFoundException error){
+            return ResponseEntity.status(404).body(new ApiResponse<>(false, error.getMessage()));
+        } catch(Exception error){
+            return ResponseEntity.status(500).body(new ApiResponse<>(false, error.getMessage()));
+        }
+    }
 
     @PutMapping("/{id}")
-    public UserDTO updateUser(@RequestBody UserRequestDTO payload, @PathVariable String id) {return userService.updateUser(id, payload);}
+    public ResponseEntity<ApiResponse<UserDTO>> updateUser(@RequestBody UserRequestDTO payload, @PathVariable String id) {
+        try {
+            return ResponseEntity.ok(new ApiResponse<>(true, userService.updateUser(id, payload)));
+        } catch(ResourceNotFoundException error){
+            return ResponseEntity.status(404).body(new ApiResponse<>(false, error.getMessage()));
+        } catch(Exception error){
+            return ResponseEntity.status(500).body(new ApiResponse<>(false, error.getMessage()));
+        }
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable String id) {
--- src/test/java/com/onlinemarket/rest/controllers/UserControllerTests.java
+++ src/test/java/com/onlinemarket/rest/controllers/UserControllerTests.java
@@ -1,33 +1,82 @@
 package com.onlinemarket.rest.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onlinemarket.core.exceptions.ApiExceptions.ConflictException;
+import com.onlinemarket.core.exceptions.general.BadRequestException;
+import com.onlinemarket.core.exceptions.repository.ResourceNotFoundException;
 import com.onlinemarket.core.service.UserService;
-import com.onlinemarket.core.service.auth.JwtService;
-import com.onlinemarket.core.service.auth.UserAuthService;
-import com.onlinemarket.rest.config.SecurityConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.authentication.AuthenticationProvider;
+import com.onlinemarket.rest.dto.user.UserRequestDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@AutoConfigureMockMvc
-@WebMvcTest(UserController.class)
-@Import(SecurityConfig.class)
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class UserControllerTests {
-    @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-    @MockBean
-    UserAuthService authService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @MockBean
+    @Mock
     UserService userService;
 
-    @MockBean
-    JwtService jwtService;
+    @InjectMocks
+    UserController userController;
 
-    @MockBean
-    AuthenticationProvider authenticationProvider;
+    String userId = "non-existent-id";
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
+    @Test
+    void shouldReturnNotFoundForNonExistentId() throws Exception{
+        when(userService.findById(any(String.class))).thenThrow(ResourceNotFoundException.class);
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/users/{id}", userId))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void shouldReturnNotFoundForUpdatingWithNonExistentId() throws Exception{
+        when(userService.updateUser(any(String.class), any(UserRequestDTO.class))).thenThrow(ResourceNotFoundException.class);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/users/{id}", userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(objectMapper.writeValueAsString(new UserRequestDTO()))
+        ).andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void shouldReturnNotFoundForDeletingWithNonExistentId() throws Exception{
+        doThrow(ResourceNotFoundException.class).when(userService).deleteUser(any(String.class));
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/users/{id}", userId))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void shouldReturnBadRequestForInvalidRequestBodyOnUpdate() throws Exception{
+        when(userService.updateUser(any(String.class), any(UserRequestDTO.class)))
+                .thenThrow(BadRequestException.class);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/users/{id}", userId))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturnConflictForUserWithAssociatedOrders() throws Exception{
+        doThrow(ConflictException.class).when(userService).deleteUser(userId);
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/users/{id}", userId))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+    }
 }
